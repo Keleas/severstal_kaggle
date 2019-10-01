6d5457b66b227759a2ae40fe7305a97a8d419d39
arinaLozhkina <32067956+arinaLozhkina@users.noreply.github.com>
2019-10-01 20:46:41 +0300

    Update and rename implement.py to classifier.py

--- [Arisha] src/binary_classifier/classifier.py
+++ [Arisha] src/binary_classifier/classifier.py
@@ -8,40 +8,60 @@ from tqdm import tqdm
 import torch.optim as optim
 import os
 import re
-from PIL import Image
+import cv2
 from torch.utils.data import Dataset
 from torchvision import transforms
 import itertools
 import math
-import numpy as np
 import torch
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 import seaborn as sns
 from collections import Counter
+from albumentations import (
+    PadIfNeeded,
+    HorizontalFlip,
+    VerticalFlip,
+    CenterCrop,
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    ElasticTransform,
+    GridDistortion,
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,
+    RandomGamma,
+    Normalize
+)
+from albumentations.torch import ToTensor
+
+labels_dict = {'[0, 0, 0, 0]':0, '[0, 0, 0, 1]':1, '[0, 0, 1, 0]':3, '[0, 1, 0, 0]': 4, '[1, 0, 0, 0]':5, '[0, 0, 1, 1]': 6,
+               '[0, 1, 1, 0]': 7, '[0, 1, 0, 1]': 8, '[1, 0, 0, 1]':9, '[1, 1, 0, 0]': 10, '[1, 0, 1, 0]': 11, '[1, 1, 1, 0]': 12,
+               '[0, 1, 1, 1]':13, '[1, 0, 1, 1]': 14, '[1, 1, 0, 1]': 15, '[1, 1, 1, 1]': 16}
 
 def labels(mode):
     files = []
     labels = []
-    count_def = 0
+    cur_labels = []
     with open(os.path.join('G:\\SteelDetection', mode + '.csv')) as csv_file:
         next(csv_file)
         for num, line in enumerate(csv_file, 1):
             line_data = re.split(r'[_,\n]', line)
             file_name = line_data[0]
-
-            if file_name not in files:
-                files.append(file_name)
-            if not not line_data[2]:
-                count_def += 1
-            if (num - 1) % 4 == 0:
-                if count_def > 0:
-                    labels.append(1)
-                else:
-                    labels.append(0)
-                count_def = 0
+            if not file_name in files:
+                files.append(line_data[0])
+            if line_data[2]:
+                cur_labels.append(1)
+            else:
+                cur_labels.append(0)
+            if (num) % 4 == 0:
+                labels.append(labels_dict[str(cur_labels)])
+                cur_labels = []
     return files, labels
-
 
 
 class SteelDataset(Dataset):
@@ -56,11 +76,19 @@ class SteelDataset(Dataset):
     def __getitem__(self, idx):
         file = self.files[idx]
         file_path = os.path.join(('G:\\SteelDetection\\' + self.mode + '_images'), file)
-        image = Image.open(file_path)
-        image = transforms.ToTensor()(image).float()
+        image = cv2.imread(file_path)
+        mean = (0.485, 0.456, 0.406)
+        std = (0.229, 0.224, 0.225)
+        train_aug = Compose([
+            VerticalFlip(p=0.5),
+            HorizontalFlip(p=0.5),
+            Normalize(mean=mean, std=std, p=1),
+            # CLAHE(p=0.5),
+            ToTensor()])
+        augmented = train_aug(image=image)
+        image = augmented['image']
         label = self.labels[idx]
         return (image, label)
-
 
 
 class Train_Model(object):
@@ -75,7 +103,7 @@ class Train_Model(object):
         self.val_data = None
 
         self.num_epochs = 100
-        self.num_classes = 2
+        self.num_classes = 17
         self.batch_size = 8
         self.learning_rate = 0.01
 
@@ -114,7 +142,6 @@ class Train_Model(object):
             with torch.set_grad_enabled(False):
                 outputs = self.model(image)
                 loss = nn.CrossEntropyLoss()(outputs, label)
-
                 losses.append(loss.item())
                 total += label.size(0)
                 _, predicted = torch.max(outputs.data, 1)
@@ -131,7 +158,11 @@ class Train_Model(object):
             self.train()
             val_loss, predicted, labels = self.validation()
             self.scheduler.step(val_loss)
-        torch.save(self.model.state_dict(), 'G:\\SteelDetection\\new_model.pth')
+        torch.save({
+            'epoch': self.num_epochs,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict()
+        }, 'G:\\SteelDetection\\model_100_ep.pth')
 
     def main(self):
         self.model = models.resnet18()
@@ -154,8 +185,7 @@ class Train_Model(object):
         train_sampler = SubsetRandomSampler(train_indices)
         valid_sampler = SubsetRandomSampler(val_indices)
 
-        self.train_loader = torch.utils.data.DataLoader(data_set, sampler=train_sampler, batch_size=self.batch_size,
-                                                   shuffle=False)
+        self.train_loader = torch.utils.data.DataLoader(data_set, sampler=train_sampler, batch_size=self.batch_size, shuffle=False)
         self.val_loader = torch.utils.data.DataLoader(data_set, sampler=valid_sampler, batch_size=self.batch_size, shuffle=False)
         self.training()
 
@@ -163,9 +193,4 @@ class Train_Model(object):
 if __name__ == '__main__':
     model = Train_Model()
     model.main()
-    # checkpoint = torch.load('G:\\SteelDetection\\new_model.pth')
-    # model.load_state_dict(checkpoint)
-
-
-
-
+    
